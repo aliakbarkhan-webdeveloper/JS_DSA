8191c34049b9c048ad7638c0c1a3343df6d0d3d0
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-08-04 04:58:03 +0500

    knapsack

--- greedyAlgorithm/fractionalKnapsack.js
+++ greedyAlgorithm/fractionalKnapsack.js
@@ -2,10 +2,9 @@
 //Questions: given the weigths and values of N items, put these items in knapsack of capacity W to get total maximum value
 
 function knapsack(arg) {
-  let values = [60, 100, 120];
-  let weigths = [10, 20, 30];
+ let items=[{weigth:10,value:60},{weigth:20,value:100},{weigth:30,value:120}]
   let capacity=50;//knapsack can hold 50 kg
 
   //find vales/weight ratio and store it in desending sorted array
-  
+
 }
