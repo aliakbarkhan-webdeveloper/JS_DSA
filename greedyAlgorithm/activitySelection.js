de212c5b0c9f89af0ef94f7f15f2065a4550caed
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-08-03 14:12:33 +0500

    activity selection

--- greedyAlgorithm/activitySelection.js
+++ greedyAlgorithm/activitySelection.js
@@ -8,18 +8,32 @@
 //Start time >= end time of next activity
 
 function activity(str) {
-
-    //at index 0 of start is representing of starting point of first activity and the oth index of end shows the ending point of that activity
-  let start = [1, 3, 0, 5, 8, 5];  //staring point of activities
-  let end = [2, 4, 6, 7, 9, 9];//ending point of activities, end time is sorted
+  //at index 0 of start is representing of starting point of first activity and the oth index of end shows the ending point of that activity
+  let start = [1, 3, 0, 5, 8, 5]; //staring point of activities
+  let end = [2, 4, 6, 7, 9, 9]; //ending point of activities, end time is sorted
 
   //maximum activities should be done at a time
 
-  let maxActivity=0;
-  let ans=[]; // activity done will be stored in this array
-
+  let maxActivity = 0;
+  let ans = []; // activity done will be stored in this array
 
   //1st activity
-  maxActivity=1;//// update the maxActivity from 0 to 1
-
+  maxActivity = 1; //// update the maxActivity from 0 to 1
+  ans.push(0);
+  let lastEnd = end[0];
+  for (let i = 0; i <= end.length; i++) {
+    if (start[i] >= lastEnd) {
+      maxActivity++;
+      ans.push(i);
+      lastEnd = end[i];
+    }
+  }
+  console.log(`max activities = ${maxActivity}`);
+  console.log("suitable activities are:");
+  for (let i = 0; i < ans.length; i++) {
+  console.log(`Activity ${ans[i]}`);
+    
+  }
 }
+
+activity()
