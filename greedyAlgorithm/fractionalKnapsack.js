d1c5db795d921b9e6f90a5b9aa2bd11324cd4e40
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-08-04 03:53:53 +0500

    knap sack

---
+++ greedyAlgorithm/fractionalKnapsack.js
@@ -0,0 +1,8 @@
+//knapsack is a bag
+//Questions: given the weigths and values of N items, put these items in knapsack of capacity W to get total maximum value
+
+function knapsack(arg) {
+  let values = [60, 100, 120];
+  let weigths = [10, 20, 30];
+  let w=50;//knapsack can hold 50 kg
+}
