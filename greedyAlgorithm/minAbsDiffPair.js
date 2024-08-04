9ffecbabfa93436cdba90d870142f91b41450e2f
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-08-04 06:12:02 +0500

    commit

---
+++ greedyAlgorithm/minAbsDiffPair.js
@@ -0,0 +1,3 @@
+//minimum absolute difference => a-b=b-a
+//Question:
+//Given two arrays of equal length, pair each element of array 1 to each element of array 2, sum of all paris difference should be minimum
