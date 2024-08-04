c0df9e74e76ff7f0c6f21e4719aef9319e2ef4fd
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-08-04 22:34:16 +0500

    bst

---
+++ trees/bst.js
@@ -0,0 +1,55 @@
+class Node {
+  constructor(data) {
+    this.value = data;
+    this.left = null;
+    this.rigth = null;
+  }
+}
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  //Checking if the root is empty or not
+  isEmpty() {
+    return this.root === null;
+  }
+  //Insert values in tree
+  insert(val) {
+    let node = new Node(val);
+    if (this.isEmpty()) {
+      this.root = node;
+      return;
+    } else {
+      this.insertNode(this.root, node);
+    }
+  }
+
+  //insert_NOde
+  insertNode(root, node) {
+    if (root.value > node.value) {
+      if (root.left === null) {
+        root.left = node;
+      } else {
+        this.insertNode(root.left, node);
+      }
+    } else {
+      if (root.rigth === null) {
+        root.rigth = node;
+      } else {
+        this.insertNode(root.rigth, node);
+      }
+    }
+  }
+
+  //Search a node
+}
+
+let bst = new BST();
+bst.insert(50);
+bst.insert(70);
+bst.insert(40);
+bst.insert(150);
+bst.insert(1);
+console.log(bst);
+console.log(bst.isEmpty());
